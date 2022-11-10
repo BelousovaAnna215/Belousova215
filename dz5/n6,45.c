226d2084df4e0f59ef18177715d64e54e83e4b7d
BelousovaAnna215 <113383599+BelousovaAnna215@users.noreply.github.com>
2022-11-10 21:46:41 +0300

    Update n6,45.c

--- dz5/n6,45.c
+++ dz5/n6,45.c
@@ -1,62 +1,63 @@
 #include<stdio.h>
 #include<stdlib.h>
+
 #include "tree45e.h"
 
-typedef struct Node2t *list2t;
-typedef struct Node2t{
-	int elem;
-	list2t left;
-	list2t right;
-} Node2t;
 
-list2t maketree(){
-	static Node2t n23 = {23,NULL,NULL};
-	static Node2t n12 = {12,&n23,NULL};
+typedef struct Nodel *listl;
+struct Nodel{
+	int elem;
+	listl left;
+	listl right;
+};
+
+
+
+stack st;
+
+listl maketree (){
+	static struct Nodel n25 = {25,NULL,NULL};
+	static struct Nodel n24 = {24,&n25,NULL};
+	static struct Nodel n23 = {23,NULL,NULL};
+	static struct Nodel n12 = {12,&n23,&n24};
 	
-	static Node2t n21 = {21,NULL,NULL};
-	static Node2t n11 = {11,&n21,NULL};
-	static Node2t n0 = {0,&n11,&n12};
-	return &n0;	
+	static struct Nodel n21 = {21,NULL,NULL};
+	static struct Nodel n11 = {11,&n21,NULL};
+	
+	static struct Nodel n0 = {0,&n11,&n12};
+	return &n0;
 }
 
-int count_level (list2t p_node, int level){
+int count_level (listl p_node, int level){
 	int cur_level = 0;
 	int amount_elem = 0;
-	if (level == 0) return 1;
-	if( p_node == NULL || level < 0)
-	return 0;
+	if (level == 0)	return 1;
+	if (p_node == NULL)	return 0;
+	putst ( &st, (void*)p_node);
 	
-	stack st = NULL;
-	//putst p_node;
+
 	while(1){
 		printf("p_node->elem = %d\n",p_node->elem);
 
 		if (cur_level == level){
 			amount_elem += 1;
-			//p_node = stack_nodes.top();
-			//stack_nodes.pop();
-			p_node = getst(&st);
-			//p_node = ll->elem;
+			p_node = (listl)getst(&st);
 			cur_level -= 1;
 		}else if(p_node->left != NULL ){
-			list2t new_p_node = p_node->left;
+			listl new_p_node = p_node->left;
 			p_node->left = NULL;
-			//stack_nodes.push(p_node);
-			st = putst ( st, (void*)p_node );
+			putst ( &st, (void*)p_node);
 			p_node = new_p_node;
 			cur_level += 1;
 		}else if(p_node->right != NULL ){
-			list2t new_p_node = p_node->right;
+			listl new_p_node = p_node->right;
 			p_node->right = NULL;
-			//stack_nodes.push(p_node);
-			st = putst ( st, (void*)p_node );
+			putst ( &st, (void*)p_node);
 			p_node = new_p_node;
 			cur_level += 1;
 		}else{
 			if(cur_level == 0) break;
-			//p_node = stack_nodes.top();
-			//stack_nodes.pop();
-			p_node = getst(&st);
+			p_node = (listl)getst(&st);
 			cur_level -= 1;
  		}
 	}	
@@ -64,10 +65,12 @@ int count_level (list2t p_node, int level){
 	return amount_elem;
 }
 
-int main(int argc, char **argv)
+int main(void)
 {
-	list p_node = maketree ();
+	listl p_node = maketree ();
 	int amount_elem = count_level (p_node, 3);
 	printf("amount_elem = %d",amount_elem);
 	return 0;
 }
+
+
